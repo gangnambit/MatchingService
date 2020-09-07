8b33f1bde1058addf9c281d4c7a7e757a63ee889
jiyoung <jiyoung4061@naver.com>
2020-09-07 15:50:44 +0900

    0907 button css

---
+++ client/src/components/views/RegisterPage/RegisterForFindingMember.js
@@ -0,0 +1,196 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {useDispatch} from 'react-redux';
+import { loginUser, registerUser } from "../../../_action/user_action";
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="/">
+          My Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+const RegisterForFindingMember = (props) => {
+
+    const classes = useStyles();
+
+  const [Email, setEmail] = useState("")
+  const [Name, setName] = useState("")
+  const [Password, setPassword] = useState("")
+  const [ConfirmPassword, setConfirmPassword] = useState("")
+
+  const dispatch = useDispatch();
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value)
+  }
+  const onEmailHandler = (event) =>{
+    setEmail(event.currentTarget.value)
+  }
+  const onPasswordHandler = (event) =>{
+    setPassword(event.currentTarget.value)
+  }
+  const onConfirmPasswordHandler = (event) =>{
+    setConfirmPassword(event.currentTarget.value)
+  }
+  const onSubmitHandler = (event) =>{
+    event.preventDefault();
+
+    if(Password !== ConfirmPassword) {
+      return alert("비밀번호가 다릅니다.")
+    }
+
+    let body = {
+      email: Email,
+      password: Password,
+      name: Name,
+    }
+
+    dispatch(registerUser(body))
+    .then(response => {
+      if(response.payload.success) {
+        props.history.push('/login')
+      } else{
+        alert('fail to register')
+      }
+    })
+
+    
+  }
+
+    return (
+        <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          회원 가입
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                value={Name}
+                onChange={onNameHandler}
+                autoComplete="name"
+                name="Name"
+                variant="outlined"
+                required
+                fullWidth
+                id="Name"
+                label="Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+              value={Email}
+              onChange={onEmailHandler}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+              value={Password}
+              onChange={onPasswordHandler}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+              value={ConfirmPassword}
+              onChange={onConfirmPasswordHandler}
+                variant="outlined"
+                required
+                fullWidth
+                name="passwordConfirm"
+                label="passwordConfirm"
+                type="password"
+                id="passwordConfirm"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {/* <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              /> */}
+            </Grid>
+          </Grid>
+          <Button
+          onClick={onSubmitHandler}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+    );
+};
+
+export default RegisterForFindingMember;
